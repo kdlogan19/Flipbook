2926b81d5f766e09eac8a5ae92c77d99b3d22b55
Kirti Desai <kirtikd19@gmail.com>
2020-09-14 09:50:23 -0400

    example Flipbook

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import HTMLFlipBook from "react-pageflip";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="flipbook-container">
+        <HTMLFlipBook width={300} height={300}>
+          <div className="demoPage-left">Page 1
+          <h1>Starting with the first page</h1>
+          
+          </div>
+          <div className="demoPage-right">
+            <p>Page 2</p>
+            <img src={logo} className="App-logo" alt="logo" />
+          </div>
+          <div className="demoPage-left">Page 3
+            <p> Third page</p>
+          </div>
+      
+          <div className="demoPage-right">Page 4
+            <p>It is fourth page</p>
+          </div>
+          
+        </HTMLFlipBook>
+      </div>
     </div>
   );
 }
